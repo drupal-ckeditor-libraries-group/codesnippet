9bea4197ebeefc91272c69d1dc594282bcffab9e
Wiktor Walc <w.walc@cksource.com>
2015-06-30 12:50:45 +0200

    Updated language files.

--- lang/gl.js
+++ lang/gl.js
@@ -3,11 +3,11 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.plugins.setLang( 'codesnippet', 'es', {
-	button: 'Insertar fragmento de código',
-	codeContents: 'Contenido del código',
-	emptySnippetError: 'Un fragmento de código no puede estar vacío.',
-	language: 'Lenguaje',
+CKEDITOR.plugins.setLang( 'codesnippet', 'gl', {
+	button: 'Inserir fragmento de código',
+	codeContents: 'Contido do código',
+	emptySnippetError: 'Un fragmento de código non pode estar baleiro.',
+	language: 'Linguaxe',
 	title: 'Fragmento de código',
 	pathName: 'fragmento de código'
 } );
